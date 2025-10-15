8c0c6c3c60bdda5ba5c3b65ac0c7d9b125efa301
Alex <133679349+alexmen656@users.noreply.github.com>
2025-10-15 15:46:05 +0200

    feat: Add AccountsView

--- src/router/index.ts
+++ src/router/index.ts
@@ -35,9 +35,9 @@ const router = createRouter({
       component: () => import('../views/PlanView.vue'),
     },
     {
-      path: '/starred',
-      name: 'starred',
-      component: () => import('../views/HomeView.vue'),
+      path: '/accounts',
+      name: 'accounts',
+      component: () => import('../views/AccountsView.vue'),
     },
     {
       path: '/trash',
