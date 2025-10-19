62befd938805dcd7767cdd8d9a643cf5e77b6317
Alex <133679349+alexmen656@users.noreply.github.com>
2025-10-19 16:05:39 +0200

    feat: Working FaceBook API!!!

--- backend/platforms/facebookAPI.js
+++ backend/platforms/facebookAPI.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import fs from 'fs/promises';
 import axios from 'axios';
+import FormData from 'form-data';
 
 dotenv.config({ path: '.env' })
 
@@ -43,11 +44,13 @@ export async function uploadVideo(videoFile, accessToken, pageId, options = {}) 
     console.log('Video file:', videoFile);
 
     try {
-        const uploadSessionId = await initializeUpload(videoFile, accessToken, pageId, options);
-        await uploadVideoChunks(videoFile, uploadSessionId, accessToken, pageId);
+        const { uploadSessionId, startOffset } = await initializeUpload(videoFile, accessToken, pageId, options);
+        await uploadVideoChunks(videoFile, uploadSessionId, startOffset, accessToken, pageId);
+        //const publishedVideo = await finishUpload(uploadSessionId, accessToken, pageId, options);
+
         const publishedVideo = await finishUpload(uploadSessionId, accessToken, pageId, options);
 
-        return publishedVideo;
+        //return publishedVideo;
     } catch (err) {
         console.error('Error during Facebook video upload:', err);
         throw err;
@@ -71,10 +74,15 @@ async function initializeUpload(videoFile, accessToken, pageId, options) {
     try {
         //ts is failinng, ts works again
         const response = await axios.post(url, null, { params });
+        //    const response = { data: { upload_session_id: '12345', start_offset: 0, end_offset: 100 } };
         console.log('Upload session created:', response.data.upload_session_id);
         console.log('Start offset:', response.data.start_offset);
         console.log('End offset:', response.data.end_offset);
-        return response.data.upload_session_id;
+        return {
+            uploadSessionId: response.data.upload_session_id,
+            startOffset: response.data.start_offset
+        };
+
     } catch (error) {
         //console.error('Error initializing upload:', error.response.data || error.message);
         console.error('Error initializing upload:');
@@ -82,20 +90,22 @@ async function initializeUpload(videoFile, accessToken, pageId, options) {
     }
 }
 
-async function uploadVideoChunks(videoFile, uploadSessionId, accessToken, pageId) {
+/*async function uploadVideoChunks(videoFile, uploadSessionId, startOffset, accessToken, pageId) {
     const apiVersion = 'v21.0';
     const url = `https://graph.facebook.com/${apiVersion}/${pageId}/videos`;
 
     try {
+        // setTimeout(async () => {
+        console.log('Uploading video chunk at offset:', startOffset);
         const fileBuffer = await fs.readFile(videoFile.path);
         const stats = await fs.stat(videoFile.path);
         const fileSize = stats.size;
 
-        const response = await axios.post(url, null, {
+        const response = /*{ data: {} } *await axios.post(url, null, {
             params: {
                 upload_phase: 'transfer',
                 upload_session_id: uploadSessionId,
-                start_offset: 0,
+                start_offset: startOffset,
                 access_token: accessToken,
                 video_file_chunk: fileBuffer.toString('base64')
             }
@@ -103,16 +113,56 @@ async function uploadVideoChunks(videoFile, uploadSessionId, accessToken, pageId
 
         console.log('Video chunks uploaded successfully:', response.data);
         return response.data;
+        // }, 8000);
     } catch (error) {
         //console.error('Error uploading video chunks:', error.response.data || error.message);
         //console.error('Error uploading video chunks:');
+
+        if (error.response?.data) {
+            //console.error(JSON.stringify(error.response.data, null, 2));
+        } else {
+            // console.error(error.message);
+        }
+
+
+        throw error;
+    }
+}*/
+
+async function uploadVideoChunks(videoFile, uploadSessionId, startOffset, accessToken, pageId) {
+    const apiVersion = 'v21.0';
+    const url = `https://graph.facebook.com/${apiVersion}/${pageId}/videos`;
+
+    try {
+        console.log('Uploading video chunk at offset:', startOffset);
+
+        const fileBuffer = await fs.readFile(videoFile.path);
+
+        const formData = new FormData();
+        formData.append('upload_phase', 'transfer');
+        formData.append('upload_session_id', uploadSessionId);
+        formData.append('start_offset', startOffset);
+        formData.append('access_token', accessToken);
+        formData.append('video_file_chunk', fileBuffer, {
+            filename: 'video.mp4',
+            contentType: 'video/mp4'
+        });
+
+        const response = await axios.post(url, formData, {
+            headers: formData.getHeaders()
+        });
+
+        console.log('Video chunk uploaded successfully:', response.data);
+
+        return response.data;
+    } catch (error) {
+        console.error('Error uploading video chunk:');
 
         if (error.response?.data) {
             console.error(JSON.stringify(error.response.data, null, 2));
         } else {
             console.error(error.message);
         }
-
 
         throw error;
     }
